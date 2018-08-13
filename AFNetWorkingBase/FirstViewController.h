ed66a6f2f0b57093e0d097cf00a36255b3b2a1c4
maobin <875153097@qq.com>
2018-08-13 15:58:39 +0800

    增加文字

---
+++ AFNetWorkingBase/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  AFNetWorkingBase
+//
+//  Created by 国投资本 on 2018/8/13.
+//  Copyright © 2018年 大麦理财. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ AFNetWorkingBase/FirstViewController.m
@@ -0,0 +1,38 @@
+//
+//  FirstViewController.m
+//  AFNetWorkingBase
+//
+//  Created by 国投资本 on 2018/8/13.
+//  Copyright © 2018年 大麦理财. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+    NSLog(@"妈的怎么传不上啊！！！！！");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
