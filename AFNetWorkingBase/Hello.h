df122b6fb68c80ee356cad4877a694715309b28f
maobin <875153097@qq.com>
2018-08-16 18:47:14 +0800

    第一次版本提交

---
+++ AFNetWorkingBase/Hello.h
@@ -0,0 +1,13 @@
+//
+//  Hello.h
+//  AFNetWorkingBase
+//
+//  Created by 国投资本 on 2018/8/16.
+//  Copyright © 2018年 大麦理财. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Hello : NSObject
+
+@end
---
+++ AFNetWorkingBase/Hello.m
@@ -0,0 +1,18 @@
+//
+//  Hello.m
+//  AFNetWorkingBase
+//
+//  Created by 国投资本 on 2018/8/16.
+//  Copyright © 2018年 大麦理财. All rights reserved.
+//
+
+#import "Hello.h"
+
+@implementation Hello
+
+-(void)dealloc{
+    
+    NSLog(@"哈哈哈哈！！");
+    
+}
+@end
