ab9ee6e83422aee566ef4ed5fc8b7d190dd7877a
maobin <875153097@qq.com>
2018-08-17 11:42:59 +0800

    分支增加了一个类

---
+++ AFNetWorkingBase/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  AFNetWorkingBase
+//
+//  Created by 国投资本 on 2018/8/17.
+//  Copyright © 2018年 大麦理财. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
